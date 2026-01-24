06e6fb414c51355cdfb616807d4d1c7bda4d23ac
AntonyBrowm <antonybrowm@gmail.com>
2026-01-24 00:38:37 -0600

    s

--- src/availability-rule/dto/update-availability-rule.dto.ts
+++ src/availability-rule/dto/update-availability-rule.dto.ts
@@ -1,4 +1,34 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateAvailabilityRuleDto } from './create-availability-rule.dto';
+import {
+  IsBoolean,
+  IsInt,
+  IsOptional,
+  IsString,
+  Min,
+  Max,
+  Matches,
+  IsArray,
+} from 'class-validator';
 
-export class UpdateAvailabilityRuleDto extends PartialType(CreateAvailabilityRuleDto) {}
+export class UpdateAvailabilityRuleDto {
+  @IsInt()
+  professionalId: number;
+
+  @IsArray()
+  @IsInt({ each: true })
+  @Min(0, { each: true })
+  @Max(6, { each: true })
+  days: number[];
+
+  @IsString()
+  @Matches(/^\d{2}:\d{2}$/)
+  startTime: string;
+
+  @IsString()
+  @Matches(/^\d{2}:\d{2}$/)
+  endTime: string;
+
+  @IsOptional()
+  @IsBoolean()
+  active?: boolean;
+  dayOfWeek: undefined;
+}
