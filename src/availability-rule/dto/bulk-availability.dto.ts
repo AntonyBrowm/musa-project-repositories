7e742e38a53a1419515fbb559aa2fe504867c481
AntonyBrowm <antonybrowm@gmail.com>
2026-01-24 00:58:09 -0600

    bulk fixed

--- src/availability-rule/dto/bulk-availability.dto.ts
+++ src/availability-rule/dto/bulk-availability.dto.ts
@@ -1,7 +1,32 @@
+import {
+  IsBoolean,
+  IsInt,
+  IsOptional,
+  IsArray,
+  Min,
+  Max,
+  Matches,
+} from 'class-validator';
+
 export class BulkAvailabilityDto {
+  @IsInt()
   professionalId: number;
-  days: number[]; // ej. [0,1,2] dayOfWeek
-  startTime?: string; // "09:00" por defecto
-  endTime?: string; // "18:00" por defecto
-  active?: boolean; // true por defecto
+
+  @IsArray()
+  @IsInt({ each: true })
+  @Min(0, { each: true })
+  @Max(6, { each: true })
+  days: number[];
+
+  @IsOptional()
+  @Matches(/^\d{2}:\d{2}$/)
+  startTime?: string;
+
+  @IsOptional()
+  @Matches(/^\d{2}:\d{2}$/)
+  endTime?: string;
+
+  @IsOptional()
+  @IsBoolean()
+  active?: boolean;
 }
